c7f8748d2d1a86e28b5b0ff9827a0e234bf7f90b
Zagura Vladimir <ZaguraVladimir@GMail.com>
2017-06-07 14:52:02 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+)
+
+
+func main() {
+	e := echo.New()
+	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
+	e.PUT("/tasks", func(c echo.Context) error { return c.JSON(200, "PUT Tasks") })
+	e.DELETE("/tasks/:id", func(c echo.Context) error { return c.JSON(200, "DELETE Task "+c.Param("id")) })
+
+	// Start as a web server
+	e.Start(":8000")
+}
