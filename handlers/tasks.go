2e1ff469fd267fca41ea7e3c5ed7adbfdc2ff712
Zagura.Vladimir <ZaguraVladimir@GMail.com>
2018-07-10 12:42:52 +0300

    10 07 2018

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -1,1 +1,32 @@
 package handlers
+
+import (
+	"database/sql"
+	"github.com/labstack/echo"
+	"net/http"
+	"strconv"
+)
+
+type H map[string]interface{}
+
+// конечная точка GetTasks
+func GetTasks(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "tasks")
+	}
+}
+
+// конечная точка PutTask
+func PutTask(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusCreated, H{"created": 123})
+	}
+}
+
+// конечная точка DeleteTask
+func DeleteTask(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
+		return c.JSON(http.StatusOK, H{"deleted": id})
+	}
+}
